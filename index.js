a41a5fa1469fb1f509539ac988f7b11d5276ac84
Daniil Kucherenko <daniil.ukraine@gmail.com>
2022-10-16 22:58:46 +0300

    GET method realisation through router added

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const service = express();
-
 const PORT = 4567;
+
+// import routes
+const employeesRoutes = require('./routes/employees');
+service.use('/employees', employeesRoutes);
+
 service.listen(PORT);
 console.log('Server is running on port ' + PORT);
 mongoose.connect('mongodb://localhost:27017/company',
